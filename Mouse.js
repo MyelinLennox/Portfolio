5a6060345d879183de43399b43d211f683441a92
ClickQuack <118149338+ClickQuack@users.noreply.github.com>
2024-02-17 19:08:31 -0600

    I have no idea what im doing

--- Mouse.js
+++ Mouse.js
@@ -1,9 +1,11 @@
 //YouTube Tutorial: https://youtu.be/wG_5453Vq98
-console.clear();
 
 // Select the circle element
-const circleElement = document.querySelector('.Cursor');
+const CircleElement = document.querySelector('.Cursor');
+const SelectableElement = document.querySelectorAll('.Selectable')
+const TestElement = document.querySelectorAll('.Test');
 
+console.log(SelectableElement)
 // Create objects to track mouse position and custom cursor position
 const mouse = { x: 0, y: 0 }; // Track current mouse position
 const previousMouse = { x: 0, y: 0 } // Store the previous mouse position
@@ -13,12 +15,21 @@ const circle = { x: 0, y: 0 }; // Track the circle position
 let currentScale = 0; // Track current scale value
 let currentAngle = 0; // Track current angle value
 let SelectionMultiplier = 1;
+let HoverMultiplier = 1;
 
 // Update mouse position on the 'mousemove' event
 window.addEventListener('mousemove', (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
 });
+
+document.addEventListener("mousedown", function () {SelectionMultiplier = .75;});
+document.addEventListener("mouseup", function () {SelectionMultiplier = 1;});
+
+document.addEventListener("mouseover", function () {HoverMultiplier = 1.5;});
+document.addEventListener("mouseout", function () {HoverMultiplier = 1;});
+
+
 
 // Smoothing factor for cursor movement speed (0 = smoother, 1 = instant)
 const speed = .4;
@@ -42,11 +53,12 @@ const tick = () => {
   // 2. Calculate mouse velocity using Pythagorean theorem and adjust speed
   const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
   // 3. Convert mouse velocity to a value in the range [0, 0.5]
-  const scaleValue = (mouseVelocity / 150) * 0.5;
+  const scaleValue = (mouseVelocity / 275) * 0.5;
   // 4. Smoothly update the current scale
   currentScale += (scaleValue - currentScale) * speed;
-  // 5. Create a transformation string for scaling
-  const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+  // Create a transformation string for scaling
+  const scaleTransform = `scale(${(1 + currentScale * HoverMultiplier) * SelectionMultiplier}, ${(1 - currentScale * HoverMultiplier) * SelectionMultiplier})`;
+
 
   // ROTATE
   // 1. Calculate the angle using the atan2 function
@@ -59,7 +71,7 @@ const tick = () => {
   const rotateTransform = `rotate(${currentAngle}deg)`;
 
   // Apply all transformations to the circle element in a specific order: translate -> rotate -> scale
-  circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform*SelectionMultiplier}`;
+  CircleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
 
   // Request the next frame to continue the animation
   window.requestAnimationFrame(tick);
